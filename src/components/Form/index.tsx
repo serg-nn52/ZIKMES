a6a45742b7af620a2f3a03486aa6f95ff279e92b
Sergey Lushkin <s.lushkin@websecret.by>
2022-11-12 17:37:21 +0300

    add adaptive layout, icon to button, bg

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,12 +1,28 @@
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import styles from './style.module.scss';
 
 const Form = () => {
   return (
-    <form className={styles.form}>
-      <input type="text" placeholder="Ваш номер..." />
-      <button type="submit">ЗАКАЗАТЬ</button>
-    </form>
+    <div className={styles.wrapper}>
+      <form className={styles.form}>
+        <label htmlFor="phoneNumber">
+          <input
+            autoComplete="off"
+            type="number"
+            id="phoneNumber"
+            placeholder="Ваш номер..."
+          />
+        </label>
+        <button type="submit">
+          <p>
+            ЗАКАЗАТЬ
+            <FontAwesomeIcon icon={faCartShopping} />
+          </p>
+        </button>
+      </form>
+    </div>
   );
 };
 
